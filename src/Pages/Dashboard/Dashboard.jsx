1caecd3820c690777a08728b7f2e83a3da0b2f8b
shafriki <shafriki.cse@gmail.com>
2025-02-22 02:57:08 +0600

    change dashboard text

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -11,10 +11,9 @@ const Dashboard = () => {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-neutral-content text-center">
                 <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                <h1 className="mb-5 text-5xl font-bold">Task Flow</h1>
                 <p className="mb-5">
-                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                    quasi. In deleniti eaque aut repudiandae et a id nisi.
+                Create tasks, edit, delete, reorder with drag-and-drop, categorize into To-Do, In Progress, Done, sync changes in real-time.
                 </p>
                 <Link to='/login'><button className="btn btn-primary">Login Now</button>
                 </Link>
